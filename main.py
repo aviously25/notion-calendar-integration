48089e62f9cab9ffa7c81a649c4b037ffea22c65
Avi Udash <udashavi@gmail.com>
2021-05-21 10:43:16 -0500

    gets list of events from notion complete

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
+import json
 import os
+from datetime import date
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -7,4 +9,20 @@ load_dotenv()
 bearer = os.getenv("BEARER")
 database_id = os.getenv("DATABASE_ID")
 
-req = requests.get("https://api.notion.com/v1/databases")
+url = "https://api.notion.com/v1/databases/{}/query".format(database_id)
+headers = {
+    "Authorization": "Bearer {}".format(bearer),
+    "Notion-Version": "2021-05-13",
+    "Content-Type": "application/json"
+}
+params = {
+    'filter': {
+        "property": "Due",
+        "date": {
+            "after": date.today().isoformat()
+        }
+    }
+}
+res = requests.post(url, headers=headers, data=json.dumps(params))
+
+print(res.json())
